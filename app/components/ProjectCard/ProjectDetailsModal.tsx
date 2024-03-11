42b98f9cddc2a4130b4f32649873ce395bf286ea
kent-daniel <kentdaniel0328@gmail.com>
2024-03-12 10:46:42 +1100

    Image optimisation

--- app/components/ProjectCard/ProjectDetailsModal.tsx
+++ app/components/ProjectCard/ProjectDetailsModal.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Modal from "../Modal";
+import Image from "next/image";
 
 type Props = {
   title: string;
@@ -21,7 +22,7 @@ const ProjectDetailsModal = ({
       <div className="flex items-center my-3">
         {images && images.length > 0 && (
           <div className="w-3/4 pr-4 overflow-hidden">
-            <img
+            <Image
               src={images[0]}
               alt="Main Image"
               className="w-full h-auto rounded-lg border"
@@ -32,7 +33,7 @@ const ProjectDetailsModal = ({
           {images &&
             images.slice(1).map((imageUrl, index) => (
               <div key={index} className="">
-                <img
+                <Image
                   src={imageUrl}
                   alt={`Image ${index + 2}`}
                   className="rounded-lg overflow-hidden mx-2 border my-1 "
