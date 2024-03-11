a0712c9859569f2b2ae0c18de4f7788082a62116
kent-daniel <kentdaniel0328@gmail.com>
2024-03-12 10:49:37 +1100

    change back to img for styling issue

--- app/components/ProjectCard/ProjectDetailsModal.tsx
+++ app/components/ProjectCard/ProjectDetailsModal.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import Modal from "../Modal";
-import Image from "next/image";
 
 type Props = {
   title: string;
@@ -22,7 +21,7 @@ const ProjectDetailsModal = ({
       <div className="flex items-center my-3">
         {images && images.length > 0 && (
           <div className="w-3/4 pr-4 overflow-hidden">
-            <Image
+            <img
               src={images[0]}
               alt="Main Image"
               className="w-full h-auto rounded-lg border"
@@ -33,7 +32,7 @@ const ProjectDetailsModal = ({
           {images &&
             images.slice(1).map((imageUrl, index) => (
               <div key={index} className="">
-                <Image
+                <img
                   src={imageUrl}
                   alt={`Image ${index + 2}`}
                   className="rounded-lg overflow-hidden mx-2 border my-1 "
